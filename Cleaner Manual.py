b3965d6f5aab353c8f1c38a2a6c567b892a8b3b6
Krakerpim <veroudepim@gmail.com>
2021-03-10 12:16:21 +0100

    Made an automatic version
    Renamed Cleaner.py to Cleaner Manual.py
    Updated the README

---
+++ Cleaner Manual.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import time
+
+### Put you file names here for the script to work
+files = ["DVS_2014-01-01.csv"]
+
+def clean_file(filepath):
+
+    ### Open the file, start timer and clear/declare variables
+    start = time.time()
+    df = pd.read_csv(filepath)
+    f = open("clean_{}.xml".format(filepath.split(".", 1)[0]), "a",encoding='utf-8')
+    clean_string = ""
+    counter = 0
+
+    ## Remove unnecesary columns and write <Root> to the xml file
+    df.drop(df.columns[0], axis=1, inplace=True) 
+    df.drop(df.columns[-1], axis=1, inplace=True)
+    f.write("<Root>"+"\n")
+
+
+    ## Itterate through all the rows and clean them up
+    for row_index in range(len(df)):
+
+        ### Select the text from the df
+        raw_text = df.iloc[row_index].values[0]
+        cleaned = ""
+
+        ## Check if certain flaws are in the text and removes them
+        if " <?xml" in raw_text:
+            cleaned = raw_text.split(""" <?xml""", 1)[0]
+            cleaned = cleaned.replace('<?xml version="1.0" encoding="UTF-8"?>', "")
+        else:
+            cleaned = raw_text.replace('<?xml version="1.0" encoding="UTF-8"?>', "")
+            cleaned = cleaned.split("""    <ns0:PutReisInformatieBoodschapIn""", 1)[0]
+
+        ## Add the ezt to a string to later write to the file
+        clean_string += cleaned + "\n"
+
+        ## If the script has itterated through 50 lines the data will be writen to the file. This is for preformance enhancment
+        if counter % 50 == 0:
+            f.write(clean_string)
+            clean_string = ""
+
+        counter += 1
+
+    ## End the file and timer
+    f.write("</Root>")
+    f.close()
+    end = time.time()
+
+    print("Converted {} to a clean XML file in: {} seconds...".format(filepath, end - start))
+
+
+if __name__ == '__main__':
+    for file in files:
+        clean_file(file)
+        os.remove(file) #This can be commented out if you do not want to remove the files after converting them
--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # CSV_XML_Cleaner
+.py 	= Visual Studio
+.ipynb 	= Jupyter Notebook
 
-## how to use the xml cleaner?
+## How to use the xml cleaner
 
-1. download the files from https://trein.fwrite.org/idx/DVS.html
-2. unzip the .xz files
-3. replace the files to the same folder as the script
-4. add the filenames to the "files" variable on the top of the script
-5. run the script
+1. Download the files from https://trein.fwrite.org/idx/DVS.html.
+2. Unzip the .xz files.
+3. Replace the files to the same folder as the script.
+4. (Manual version only) Add the filenames to the "files" variable on the top of the script.
+5. If you do not want to have the source files removed you have to remove the last line.
+6. Run the script.
